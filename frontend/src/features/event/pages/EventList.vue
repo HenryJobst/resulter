32196a0ec9c5fc0bd7f6523715eb75ed0ad410d9
Henry Jobst <github@jobst-berlin.de>
2023-12-17 21:47:56 +0100

    Korrekturen Mehrsprachigkeit

--- frontend/src/features/event/pages/EventList.vue
+++ frontend/src/features/event/pages/EventList.vue
@@ -1,70 +1,53 @@
 <script setup lang="ts">
 import { useEventStore } from '@/features/event/store/event.store'
 import Button from 'primevue/button'
-import SelectButton from 'primevue/selectbutton'
 import DataTable from 'primevue/datatable'
 import Column from 'primevue/column'
-
-import { ref } from 'vue'
 import Spinner from '@/components/SpinnerComponent.vue'
 import ErrorMessage from '@/components/ErrorMessage.vue'
+import { useI18n } from 'vue-i18n'
 
-interface Size {
-  label: string
-  value: string
-  class?: string
-}
+const { t } = useI18n() // same as `useI18n({ useScope: 'global' })`
 
 const store = useEventStore()
-const size = ref<Size>({
-  label: 'Normal',
-  value: 'normal'
-})
-
-const sizeOptions = ref([
-  { label: 'Small', value: 'small', class: 'sm' },
-  { label: 'Normal', value: 'normal' },
-  { label: 'Large', value: 'large', class: 'lg' }
-])
 </script>
 
 <template>
-  <h2>Wettkämpfe</h2>
+  <h1>{{ t('labels.event') }}</h1>
   <div class="flex justify-content-between my-4">
     <router-link
       :to="{
         name: 'event-new'
       }"
     >
-      <Button label="New"></Button>
+      <Button :label="t('labels.new')"></Button>
     </router-link>
-    <Button severity="secondary" label="Neu laden" @click="store.loadEventsAction"></Button>
+    <Button
+      severity="secondary"
+      :label="t('labels.reload')"
+      @click="store.loadEventsAction"
+    ></Button>
   </div>
 
   <ErrorMessage :message="store.errorMessage"></ErrorMessage>
   <Spinner v-if="store.loadingEvents"></Spinner>
 
   <div v-if="store.errorMessage === null && !store.loadingEvents">
-    <div class="flex justify-content-center mb-4">
-      <SelectButton v-model="size" :options="sizeOptions" optionLabel="label" dataKey="label" />
-    </div>
-    <DataTable
-      :value="store.events"
-      :class="`p-datatable-${size.class}`"
-      tableStyle="min-width: 50rem"
-    >
-      <Column field="id" header="#"></Column>
-      <Column field="name" header="Name"></Column>
-      <!--Column class="text-right" field="volume" header="Volume"></Column-->
+    <DataTable :value="store.events" class="p-datatable-sm" tableStyle="min-width: 50rem">
+      <Column field="id.value" header="#" />
+      <Column field="name.value" :header="t('labels.name')" />
+      <Column field="startDate.value" :header="t('labels.date')" />
+      <Column class="text-right" field="classResults.size" :header="t('labels.class', 2)" />
+      <Column class="text-right" field="classResults.size" :header="t('labels.participant', 2)" />
       <Column class="text-right">
         <template #body="slotProps">
           <router-link :to="{ name: 'event-edit', params: { id: slotProps.data.id } }">
-            <Button icon="pi pi-pencil" class="mr-2" label="Bearbeiten"></Button>
+            <Button icon="pi pi-pencil" class="mr-2" :label="t('labels.edit')"></Button>
           </router-link>
           <Button
             icon="pi pi-trash"
             severity="danger"
-            label="Löschen"
+            :label="t('labels.delete')"
             @click="store.deleteEventAction(slotProps.data.id)"
           ></Button>
         </template>

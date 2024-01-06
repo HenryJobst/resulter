ebe1d8280adfbe7505aef03fac67299019db2b18
Henry Jobst <github@jobst-berlin.de>
2024-01-06 10:44:01 +0100

    Cup: refresh cup list after cup editing

--- frontend/src/features/cup/pages/CupEdit.vue
+++ frontend/src/features/cup/pages/CupEdit.vue
@@ -5,9 +5,8 @@ import { useAuthStore } from '@/features/keycloak/store/auth.store'
 import CupForm from '@/features/cup/widgets/CupForm.vue'
 import type { Cup } from '@/features/cup/model/cup'
 import Button from 'primevue/button'
-import { useMutation, useQuery } from '@tanstack/vue-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/vue-query'
 import { CupService } from '@/features/cup/services/cup.service'
-import { computed } from 'vue'
 import ErrorMessage from '@/components/ErrorMessage.vue'
 import Spinner from '@/components/SpinnerComponent.vue'
 
@@ -16,23 +15,26 @@ const authStore = useAuthStore()
 
 const { t } = useI18n()
 
-const { status, data, loadError } = useQuery({
-  queryKey: ['cup', { id: props.id }],
+const cupQuery = useQuery({
+  queryKey: ['cups', props.id],
   queryFn: () => CupService.getById(props.id, t)
 })
 
-const cup = computed(() => {
-  return data.value
-})
+const queryClient = useQueryClient()
 
-const { isPending, isError, updateError, isSuccess, mutate } = useMutation({
-  mutationFn: (cup: Cup) => CupService.update(cup, t)
+queryClient.invalidateQueries({ queryKey: ['cups', props.id] })
+
+const cupMutation = useMutation({
+  mutationFn: (cup: Cup) => CupService.update(cup, t),
+  onSuccess: () => {
+    queryClient.invalidateQueries({ queryKey: ['cups'] })
+  }
 })
 
 const router = useRouter()
 
 const cupSubmitHandler = (cup: Cup) => {
-  mutate(cup)
+  cupMutation.mutate(cup)
   router.back()
 }
 </script>
@@ -40,14 +42,14 @@ const cupSubmitHandler = (cup: Cup) => {
 <template>
   <div v-bind="$attrs">
     <h2>{{ t('messages.edit_cup', { id: props.id }) }}</h2>
-    <span v-if="status.value === 'pending'">
+    <span v-if="cupQuery.status.value === 'pending'">
       {{ t('messages.loading') }}
       <Spinner />
     </span>
-    <span v-else-if="status.value === 'error'">
-      <ErrorMessage :message="t('messages.error', { message: loadError.value?.message })" />
+    <span v-else-if="cupQuery.status.value === 'error'">
+      <ErrorMessage :message="t('messages.error', { message: cupQuery.error.value?.message })" />
     </span>
-    <CupForm :cup="cup" @cup-submit="cupSubmitHandler" v-else-if="data">
+    <CupForm :cup="cupQuery.data.value" @cup-submit="cupSubmitHandler" v-else-if="cupQuery.data">
       <Button
         v-if="authStore.isAdmin"
         class="mt-2"

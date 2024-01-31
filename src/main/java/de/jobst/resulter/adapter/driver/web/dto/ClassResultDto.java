077078235390797a0984ba4a912589d94dfcea7e
Henry Jobst <github@jobst-berlin.de>
2024-01-31 22:31:55 +0100

    backend: make ClassResultDto comparable

--- src/main/java/de/jobst/resulter/adapter/driver/web/dto/ClassResultDto.java
+++ src/main/java/de/jobst/resulter/adapter/driver/web/dto/ClassResultDto.java
@@ -1,13 +1,20 @@
 package de.jobst.resulter.adapter.driver.web.dto;
 
 import de.jobst.resulter.domain.ClassResult;
+import org.springframework.lang.NonNull;
 
 import java.util.Collection;
 
-public record ClassResultDto(String name, Collection<PersonResultDto> personResults) {
+public record ClassResultDto(String name, Collection<PersonResultDto> personResults)
+    implements Comparable<ClassResultDto> {
 
     static public ClassResultDto from(ClassResult classResult) {
         return new ClassResultDto(classResult.classResultName().value(),
             classResult.personResults().value().stream().map(PersonResultDto::from).sorted().toList());
+    }
+
+    @Override
+    public int compareTo(@NonNull ClassResultDto o) {
+        return name.compareTo(o.name);
     }
 }

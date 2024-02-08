3b0b2958808c26196a2a17a622fbc5938084b85f
Henry Jobst <github@jobst-berlin.de>
2024-02-08 23:30:38 +0100

    frontend: bunp version to 0.0.2

--- frontend/build.sh
+++ frontend/build.sh
@@ -5,4 +5,4 @@ source .env.production
 set +o allexport
 
 # build
-docker build --build-arg="VITE_API_ENDPOINT=${VITE_API_ENDPOINT}" --build-arg="VITE_KEYCLOAK_URL=${VITE_KEYCLOAK_URL}" --build-arg="VITE_KEYCLOAK_CLIENT_ID=${VITE_KEYCLOAK_CLIENT_ID}" --build-arg="VITE_KEYCLOAK_REALM=${VITE_KEYCLOAK_REALM}" -t resulter:0.0.1 .
+docker build --build-arg="VITE_API_ENDPOINT=${VITE_API_ENDPOINT}" --build-arg="VITE_KEYCLOAK_URL=${VITE_KEYCLOAK_URL}" --build-arg="VITE_KEYCLOAK_CLIENT_ID=${VITE_KEYCLOAK_CLIENT_ID}" --build-arg="VITE_KEYCLOAK_REALM=${VITE_KEYCLOAK_REALM}" -t resulter:0.0.2 .

0110b11c36c906bc12f32f934da4edc134d9801d
Henry Jobst <github@jobst-berlin.de>
2024-03-04 21:44:13 +0100

    build(backend): set max page size and default page size (for persons and organisations) to 5000

--- src/main/java/de/jobst/resulter/adapter/driver/web/OrganisationController.java
+++ src/main/java/de/jobst/resulter/adapter/driver/web/OrganisationController.java
@@ -10,6 +10,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -38,6 +39,7 @@ public class OrganisationController {
 
     @GetMapping("/organisation")
     public ResponseEntity<Page<OrganisationDto>> searchOrganisations(@RequestParam Optional<String> filter,
+                                                                     @PageableDefault(page = 0, size = 5000)
                                                                      Pageable pageable) {
         try {
             Page<Organisation> organisations = organisationService.findAll(filter.orElse(null),
--- src/main/java/de/jobst/resulter/adapter/driver/web/PersonController.java
+++ src/main/java/de/jobst/resulter/adapter/driver/web/PersonController.java
@@ -11,6 +11,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -41,7 +42,8 @@ public class PersonController {
     }
 
     @GetMapping("/person")
-    public ResponseEntity<Page<PersonDto>> searchPersons(@RequestParam Optional<String> filter, Pageable pageable) {
+    public ResponseEntity<Page<PersonDto>> searchPersons(@RequestParam Optional<String> filter,
+                                                         @PageableDefault(page = 0, size = 5000) Pageable pageable) {
         try {
             Page<Person> persons = personService.findAll(filter.orElse(null),
                 pageable != null ?

8fc77fd6cdaad45b2dcc4324fbaa68a51cd29f11
Henry Jobst <github@jobst-berlin.de>
2024-03-06 23:05:42 +0100

    refactor(backend): drop BlankCertificate, EventCertificate use a MediaFile as blankCertificate attribute

--- src/main/java/de/jobst/resulter/domain/EventCertificate.java
+++ src/main/java/de/jobst/resulter/domain/EventCertificate.java
@@ -1,22 +1,28 @@
 package de.jobst.resulter.domain;
 
+import lombok.Getter;
 import lombok.Setter;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 
+@Getter
+@Setter
 public class EventCertificate {
 
     @NonNull
-    @Setter
     private EventCertificateId id;
 
     @Nullable
     private String textParagraphsAsJson;
 
     @Nullable
-    private BlankCertificate blankCertificate;
+    private MediaFile blankCertificate;
 
-    public EventCertificate(@NonNull EventCertificateId id) {
+    public EventCertificate(@NonNull EventCertificateId id,
+                            @Nullable String textParagraphsAsJson,
+                            @Nullable MediaFile blankCertificate) {
         this.id = id;
+        this.textParagraphsAsJson = textParagraphsAsJson;
+        this.blankCertificate = blankCertificate;
     }
 }

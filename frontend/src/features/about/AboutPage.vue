7f3e27afb3f17fb44827f08fe46411da54d41644
Henry Jobst <github@jobst-berlin.de>
2024-03-29 13:11:54 +0100

    feat(frontend): add simple about page content based on README

--- frontend/src/features/about/AboutPage.vue
+++ frontend/src/features/about/AboutPage.vue
@@ -5,7 +5,28 @@ const { t } = useI18n() // same as `useI18n({ useScope: 'global' })`
 </script>
 
 <template>
-    <h2>{{ t('pages.about') }}</h2>
+    <div class="about">
+        <div class="max-w-4xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+            <h1 class="text-3xl font-extrabold text-gray-900 mb-4">
+                {{ t('about.title') }}
+            </h1>
+            <p class="mt-2 text-lg text-gray-600">
+                {{ t('about.description') }}
+            </p>
+            <p class="mt-2 text-lg text-gray-600">
+                {{ t('about.features') }}
+            </p>
+            <p class="mt-2 text-lg text-gray-600">
+                {{ t('about.futurePlans') }}
+            </p>
+            <p class="mt-8 text-lg text-gray-900">
+                {{ t('about.contact') }}
+                <a :href="`mailto:${t('about.emailAddress')}`" class="text-indigo-600 hover:text-indigo-900">
+                    {{ t('about.emailAddress') }}
+                </a>
+            </p>
+        </div>
+    </div>
 </template>
 
 <style></style>

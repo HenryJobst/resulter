f211f67e30c3740532623dc7452bc910c55ccb1b
Henry Jobst <github@jobst-berlin.de>
2024-05-26 00:21:23 +0200

    feat(backend): improve error handling when creating a certificate with invalid layout description

--- src/main/java/de/jobst/resulter/application/certificate/JsonSchemaValidator.java
+++ src/main/java/de/jobst/resulter/application/certificate/JsonSchemaValidator.java
@@ -1,8 +1,11 @@
 package de.jobst.resulter.application.certificate;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import com.github.fge.jsonschema.core.report.ListProcessingReport;
+import com.github.fge.jsonschema.core.report.ProcessingMessage;
 import com.github.fge.jsonschema.core.report.ProcessingReport;
 import com.github.fge.jsonschema.main.JsonSchemaFactory;
 import com.github.fge.jsonschema.main.JsonValidator;
@@ -21,6 +24,16 @@ public class JsonSchemaValidator {
             JsonValidator validator = factory.getValidator();
 
             return validator.validate(schemaNode, jsonNode, true);
+        } catch (JsonParseException e) {
+            ProcessingReport report = new ListProcessingReport();
+            ProcessingMessage message = new ProcessingMessage();
+            message.setMessage("Failed to parse json: " + e.getMessage());
+            try {
+                report.error(message);
+            } catch (ProcessingException ex) {
+                throw new RuntimeException(ex);
+            }
+            return report;
         } catch (IOException | ProcessingException e) {
             throw new RuntimeException("Failed to validate json against schema", e);
         }

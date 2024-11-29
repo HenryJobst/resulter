ec4e8a5459b216fa91ca66fcca6cadfa445b5630
Henry Jobst <github@jobst-berlin.de>
2024-11-29 23:24:40 +0100

    fix(frontend/backend): enable to set margins for certificates

--- src/main/java/de/jobst/resulter/application/certificate/DocumentDefinition.java
+++ src/main/java/de/jobst/resulter/application/certificate/DocumentDefinition.java
@@ -1,3 +1,4 @@
 package de.jobst.resulter.application.certificate;
 
-public record DocumentDefinition(String font, String boldFont, String italicFont, String boldItalicFont) {}
+public record DocumentDefinition(MarginsDefinition margins, String font, String boldFont, String italicFont,
+                                 String boldItalicFont) {}
---
+++ src/main/java/de/jobst/resulter/application/certificate/MarginsDefinition.java
@@ -0,0 +1,4 @@
+package de.jobst.resulter.application.certificate;
+
+public record MarginsDefinition(Float top, Float bottom, Float left,
+                                Float right) {}

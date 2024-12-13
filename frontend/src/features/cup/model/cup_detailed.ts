cf461b333b03de3eb459ad2e8370a24a5dc6948c
Henry Jobst <github@jobst-berlin.de>
2024-12-13 15:17:39 +0100

    fix(frontend/backend): further implementing nor cup results calculation and view

--- frontend/src/features/cup/model/cup_detailed.ts
+++ frontend/src/features/cup/model/cup_detailed.ts
@@ -2,6 +2,7 @@ import type { CupType } from '@/features/cup/model/cuptype'
 import type { EventKey } from '@/features/event/model/event_key'
 import type { EventRacesCupScore } from '@/features/cup/model/event_races_cup_score'
 import type { OrganisationScore } from '@/features/cup/model/organisation_score'
+import type { PersonWithScore } from '@/features/cup/model/person_with_score'
 
 export interface CupDetailed {
     id: number
@@ -10,4 +11,5 @@ export interface CupDetailed {
     events: EventKey[]
     eventRacesCupScores: EventRacesCupScore[]
     overallOrganisationScores: OrganisationScore[]
+    classResultShortNameScores: Map<string, PersonWithScore[]>
 }
--- src/main/java/de/jobst/resulter/adapter/driver/web/dto/CupDetailedDto.java
+++ src/main/java/de/jobst/resulter/adapter/driver/web/dto/CupDetailedDto.java
@@ -1,15 +1,19 @@
 package de.jobst.resulter.adapter.driver.web.dto;
 
+import de.jobst.resulter.domain.ClassResultShortName;
 import de.jobst.resulter.domain.CupDetailed;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ObjectUtils;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 public record CupDetailedDto(Long id, String name, CupTypeDto type, List<EventKeyDto> events,
                              List<EventRacesCupScoreDto> eventRacesCupScores,
-                             List<OrganisationScoreDto> overallOrganisationScores) {
+                             List<OrganisationScoreDto> overallOrganisationScores,
+                             Map<ClassResultShortName, List<PersonWithScoreDto>> classResultShortNameScores) {
 
     static public CupDetailedDto from(CupDetailed cup) {
         return new CupDetailedDto(ObjectUtils.isNotEmpty(cup.getId()) ? cup.getId().value() : 0,
@@ -21,8 +25,12 @@ public record CupDetailedDto(Long id, String name, CupTypeDto type, List<EventKe
                 .stream()
                 .map(entry -> new OrganisationScoreDto(OrganisationDto.from(entry.organisation()),
                     entry.score(),
-                    entry.personWithScores().stream().map(PersonWithScoreDto::from).toList()
-                ))
-                .toList());
+                    entry.personWithScores().stream().map(PersonWithScoreDto::from).toList()))
+                .toList(),
+            cup.getClassResultShortNameScores()
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getKey,
+                    it -> it.getValue().stream().map(PersonWithScoreDto::from).toList())));
     }
 }

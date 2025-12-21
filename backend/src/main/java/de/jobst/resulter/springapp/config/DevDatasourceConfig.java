3715737da4ac4355e068b48a55d9b850a569c145
Henry Jobst <github@jobst-berlin.de>
2025-12-21 21:40:21 +0100

    fix: update p6spy

--- backend/src/main/java/de/jobst/resulter/springapp/config/DevDatasourceConfig.java
+++ backend/src/main/java/de/jobst/resulter/springapp/config/DevDatasourceConfig.java
@@ -16,10 +16,10 @@ public class DevDatasourceConfig {
     @Primary
     public DataSource dataSource() {
         return DataSourceBuilder.create()
-            .url("jdbc:postgresql://localhost:5432/resulter")
+            .url("jdbc:p6spy:postgresql://localhost:5432/resulter")
             .username("resulter")
             .password("resulter")
-            .driverClassName("org.postgresql.Driver")
+            .driverClassName("com.p6spy.engine.spy.P6SpyDriver")
             .build();
     }
 }

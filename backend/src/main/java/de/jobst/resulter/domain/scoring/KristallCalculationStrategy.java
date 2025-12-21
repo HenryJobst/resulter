6fb994652298eddda4da22ba01065826a447b029
Henry Jobst <github@jobst-berlin.de>
2025-12-21 22:22:40 +0100

    fix: KristallCalculationStrategy: exclude "Volkssport Berlin"

--- backend/src/main/java/de/jobst/resulter/domain/scoring/KristallCalculationStrategy.java
+++ backend/src/main/java/de/jobst/resulter/domain/scoring/KristallCalculationStrategy.java
@@ -11,7 +11,7 @@ public class KristallCalculationStrategy implements CupTypeCalculationStrategy {
     @Nullable private final Map<OrganisationId, Organisation> organisationById;
 
     Set<String> classesToSkip = Set.of("BK", "BL", "Beg", "Trim", "Beginner", "OffK", "OffL", "D/H-12 Be");
-    Set<String> organisationsToSkip = Set.of("ohne", "Volkssport");
+    Set<String> organisationsToSkip = Set.of("ohne", "Volkssport", "Volkssport Berlin");
 
     public KristallCalculationStrategy(@Nullable Map<OrganisationId, Organisation> organisationById) {
         this.organisationById = organisationById;

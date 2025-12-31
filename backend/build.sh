5d03c41efbbc224317cc20810d7b35da68dceab5
Henry Jobst <github@jobst-berlin.de>
2025-12-31 11:19:01 +0100

    fix: add new env vars to docker build

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,8 @@
 DOCKER_BUILDKIT=1 docker build \
     --build-arg API_OAUTH2_RESOURCE_SERVER_JWT_ISSUER_URI \
     --build-arg API_OAUTH2_RESOURCE_SERVER_JWT_JWK_SET_URI \
+    --build-arg BFF_OAUTH2_CLIENT_ID \
+    --build-arg BFF_OAUTH2_CLIENT_SECRET \
     --build-arg API_CORS_ALLOWED_ORIGINS \
     --build-arg RESULTER_DATABASE_HOST \
     --build-arg RESULTER_DATABASE_PORT \

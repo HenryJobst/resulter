5e623e4737124b1625c2f2e016edc7537e5837dc
Henry Jobst <github@jobst-berlin.de>
2026-01-14 00:03:39 +0100

    feat: US-007 - Optimize EventRepositoryDataJdbcAdapter.findAll(filter, pageable)

--- backend/src/main/java/de/jobst/resulter/adapter/driven/jdbc/EventCertificateJdbcRepository.java
+++ backend/src/main/java/de/jobst/resulter/adapter/driven/jdbc/EventCertificateJdbcRepository.java
@@ -2,10 +2,13 @@ package de.jobst.resulter.adapter.driven.jdbc;
 
 import java.util.Collection;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.data.jdbc.core.mapping.AggregateReference;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
 import org.springframework.stereotype.Repository;
 
@@ -20,4 +23,7 @@ public interface EventCertificateJdbcRepository
     Optional<EventCertificateDbo> findByEventAndPrimary(AggregateReference<EventDbo, Long> event, boolean primary);
 
     Collection<EventCertificateDbo> findAllByEvent(AggregateReference<EventDbo, Long> event);
+
+    @Query("SELECT * FROM event_certificate WHERE event_id IN (:eventIds) AND \"primary\" = true")
+    Collection<EventCertificateDbo> findPrimaryByEventIdIn(@Param("eventIds") Set<Long> eventIds);
 }
